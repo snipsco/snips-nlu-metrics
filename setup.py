f4c12298f35f38678aca5a5a329683fd7ebe05a2
Adrien Ball <adrien.ball@snips.net>
2017-08-04 19:37:07 +0200

    Fix packaging data

--- setup.py
+++ setup.py
@@ -9,26 +9,28 @@ PACKAGE_NAME = "nlu_metrics"
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.join(ROOT_PATH, PACKAGE_NAME)
 VERSION = "__version__"
+DEFAULT_TRAINING_VERSION = "__default_training_version__"
+DEFAULT_INFERENCE_VERSION = "__default_inference_version__"
 
 with io.open(os.path.join(PACKAGE_PATH, VERSION)) as f:
     version = f.readline().strip()
 
-with io.open(os.path.join(PACKAGE_PATH, "__default_training_version__")) as f:
+with io.open(os.path.join(PACKAGE_PATH, DEFAULT_TRAINING_VERSION)) as f:
     training_version = f.readline().strip()
 
-with io.open(os.path.join(PACKAGE_PATH, "__default_inference_version__")) as f:
+with io.open(os.path.join(PACKAGE_PATH, DEFAULT_INFERENCE_VERSION)) as f:
     inference_version = f.readline().strip()
 
 install_requires = [
     "requests==2.18.2",
-    "pymongo==3.4.0"
+    "pymongo==3.4.0",
+    "snips_nlu==%s" % training_version,
+    "snips_nlu_rust==%s" % inference_version,
 ]
 
 extras_require = {
     "test": [
         "mock==2.0.0",
-        "snips_nlu==%s" % training_version,
-        "snips_nlu_rust==%s" % inference_version,
     ]
 }
 
@@ -43,6 +45,8 @@ setup(name=PACKAGE_NAME,
       package_data={
           "": [
               VERSION,
+              DEFAULT_TRAINING_VERSION,
+              DEFAULT_INFERENCE_VERSION
           ]},
       include_package_data=True,
       zip_safe=False)
